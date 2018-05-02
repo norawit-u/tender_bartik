24043cfd702d6af6d53f6a6a9efe8b5947ad58d0
Norawit Urailertprasert <norawit543@hotmail.com>
2018-05-03 02:08:47 +0700

    add feild to object

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'fname', 'lname', 'address', 'telno', 'department',
-        'email', 'password', 'fb', 'ig', 'line', 'role', 'image_path'
+        'email', 'password', 'fb', 'ig', 'line', 'role', 'image_path', 'supervisor_id'
     ];
 
     /**
